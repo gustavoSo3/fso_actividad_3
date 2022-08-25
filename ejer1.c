ea89633b5bd2ac6c06d65a9a1167817a506deddd
Gustavo Sotres <gus.sotres@gmail.com>
2022-08-25 19:22:47 +0000

    Completed exercise 1

--- ejer1.c
+++ ejer1.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main()
 {
+	pid_t child_pid = fork();
+
+	if (child_pid == 0)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			printf("Soy el hijo\n");
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			printf("Soy el padre\n");
+		}
+		wait(NULL);
+		printf("Mi proceso hijo ha terminado\n");
+	}
 
 	return 0;
 }
