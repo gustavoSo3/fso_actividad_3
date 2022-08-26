9860ea14d1ae452b8226af1d72fb9741dce2a712
Gustavo Sotres <gus.sotres@gmail.com>
2022-08-26 16:49:29 +0000

    Completed exercise 3

--- ejer3.c
+++ ejer3.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 
+	int n = atoi(argv[1]);
+	for (int i = 1; i <= n; i++)
+	{
+		pid_t p = fork();
+		if (p == 0)
+		{
+			printf("Proceso hijo %d\n", i);
+			exit(0);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	printf("FIN\n");
 	return 0;
 }
