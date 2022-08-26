e79437da64552f212d847f7f3b40ff3700d164ff
Gustavo Sotres <gus.sotres@gmail.com>
2022-08-26 17:25:29 +0000

    Completed exercise 4

--- ejer4.c
+++ ejer4.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main()
 {
+	char program_name[20];
 
+	while (1)
+	{
+		printf("Enter the file name:");
+		scanf("%s", program_name);
+		if (strcmp(program_name, "exit") == 0)
+		{
+			break;
+		}
+		system(program_name);
+	}
 	return 0;
 }
